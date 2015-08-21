7526fe8e3b7e6d719e9c32f0f4c83b146ddbff98
Josh <jephillips@cohesionforce.com>
2015-08-20 22:54:03 -0500

    Now iterating over data to generate question nodes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var App = React.createClass({
     getInitialState: function () {
         return {
-            q1: '',
-            q2: ''
+            questions: [
+                {id: "q1", question: "1 + 1", answer: "2"},
+                {id: "q2", question: "5 x 5", answer: "25"},
+                {id: "q3", question: "20 / 4", answer: "5"}
+            ]
         }
     },
     submit: function () {
@@ -10,59 +13,65 @@ var App = React.createClass({
 
         this.refs.q1.correct ? score++ : console.log("Question 1 Wrong");
         this.refs.q2.correct ? score++ : console.log("Question 2 Wrong");
+        this.refs.q3.correct ? score++ : console.log("Question 3 Wrong");
 
         alert("You scored " + score);
     },
     render: function () {
         return (
+
             <div>
                 <h1> A simple react quiz </h1>
                 <hr />
-                <Question ref="q1"
-                          question="1+1"
-                          answer="2"/>
-                <Question ref="q2"
-                          question="5 x 5"
-                          answer="25"/>
-                <button onClick={this.submit}>{'Submit'}</button>
+                {this.state.questions.map(function (question) {
+
+                    return (
+                        <div>
+                            <Question ref={question.id}
+                                      question={question.question}
+                                      answer={question.answer}/>
+                        </div>
+                    )
+                })}
+            <button onClick={this.submit}>{'Submit'}</button>
             </div>
-        );
+                );
 
-    }
-});
+                }
+                });
 
-var Question = React.createClass({
-    propTypes: {
-        correct: React.PropTypes.bool,
-        question: React.PropTypes.string.isRequired,
-        answer: React.PropTypes.string.isRequired,
-        guess: React.PropTypes.string
-    },
-    getDefaultProps: function () {
-        return {
-            question: '',
-            answer: '',
-            guess: ''
-        }
-    },
-    verify: function(e){
-        this.guess = e.target.value;
-        this.correct =  this.props.answer === this.guess;
-    }
-    ,
-    render: function () {
-        return (
-            <div>
+                var Question = React.createClass({
+                propTypes: {
+                correct: React.PropTypes.bool,
+                question: React.PropTypes.string.isRequired,
+                answer: React.PropTypes.string.isRequired,
+                guess: React.PropTypes.string
+            },
+                getDefaultProps: function () {
+                return {
+                question: '',
+                answer: '',
+                guess: ''
+            }
+            },
+                verify: function(e){
+                this.guess = e.target.value;
+                this.correct =  this.props.answer === this.guess;
+            }
+                ,
+                render: function () {
+                return (
+                <div>
                 <label>{this.props.question} </label>
                 <br />
                 <input ref="inp"
-                       type="text"
-                       question={this.props.question}
-                       answer={this.props.answer}
-                       onChange={this.verify}/>
-            </div>
-        );
-    }
-});
+                type="text"
+                question={this.props.question}
+                answer={this.props.answer}
+                onChange={this.verify}/>
+                </div>
+                );
+            }
+            });
 
-React.render(<App />, document.body);
+                React.render(<App />, document.body);
